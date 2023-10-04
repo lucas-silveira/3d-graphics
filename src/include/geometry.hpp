ff9c6849c3f6f9c61f0f4bf37c25affbb8d02ebf
Lucas Silveira <lugano1995@gmail.com>
2023-10-04 11:40:57 -0300

    refactor

--- src/include/geometry.hpp
+++ src/include/geometry.hpp
@@ -1,3 +1,6 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
 #include <SFML/Graphics.hpp>
 #include <vector>
 
@@ -26,23 +29,25 @@ struct mat4x4
     float m[4][4] = { 0 };
 };
 
-mat4x4 makeIdentityMatrix();
-mat4x4 makeRotatedMatrixX(float angleRad);
-mat4x4 makeRotatedMatrixY(float angleRad);
-mat4x4 makeRotatedMatrixZ(float angleRad);
-mat4x4 makeTranslatedMatrix(float x, float y, float z);
-mat4x4 makeProjectionMatrix(float fovDegrees, float aspectRatio, float near, float far);
-mat4x4 mulMatrices(mat4x4 &m1, mat4x4 &m2);
-mat4x4 pointAt(vec3d &pos, vec3d &forw, vec3d &up);
-mat4x4 quickInverse(mat4x4 &m);
-vec3d mulMatrixByVector(mat4x4 &m, vec3d &i);
-vec3d addVectors(vec3d &v1, vec3d &v2);
-vec3d subVectors(vec3d &v1, vec3d &v2);
-vec3d mulVector(vec3d &v1, float k);
-vec3d divVector(vec3d &v1, float k);
-float dotProduct(vec3d &v1, vec3d &v2);
-float lenVector(vec3d &v);
-vec3d normVector(vec3d &v);
-vec3d crossProduct(vec3d &v1, vec3d &v2);
-vec3d intersectPlane(vec3d &planeP, vec3d &planeN, vec3d &lineStart, vec3d&lineEnd);
-int clipAgainstPlane(vec3d planeP, vec3d planeN, triangle &inTri, triangle &outTri1, triangle &outTri2);
+extern mat4x4 makeIdentityMatrix();
+extern mat4x4 makeRotatedMatrixX(float angleRad);
+extern mat4x4 makeRotatedMatrixY(float angleRad);
+extern mat4x4 makeRotatedMatrixZ(float angleRad);
+extern mat4x4 makeTranslatedMatrix(float x, float y, float z);
+extern mat4x4 makeProjectionMatrix(float fovDegrees, float aspectRatio, float near, float far);
+extern mat4x4 mulMatrices(mat4x4 &m1, mat4x4 &m2);
+extern mat4x4 pointAt(vec3d &pos, vec3d &forw, vec3d &up);
+extern mat4x4 quickInverse(mat4x4 &m);
+extern vec3d mulMatrixByVector(mat4x4 &m, vec3d &i);
+extern vec3d addVectors(vec3d &v1, vec3d &v2);
+extern vec3d subVectors(vec3d &v1, vec3d &v2);
+extern vec3d mulVector(vec3d &v1, float k);
+extern vec3d divVector(vec3d &v1, float k);
+extern float dotProduct(vec3d &v1, vec3d &v2);
+extern float lenVector(vec3d &v);
+extern vec3d normVector(vec3d &v);
+extern vec3d crossProduct(vec3d &v1, vec3d &v2);
+extern vec3d intersectPlane(vec3d &planeP, vec3d &planeN, vec3d &lineStart, vec3d&lineEnd);
+extern int clipAgainstPlane(vec3d planeP, vec3d planeN, triangle &inTri, triangle &outTri1, triangle &outTri2);
+
+#endif
